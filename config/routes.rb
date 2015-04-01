83a7e06f94ec99bb9c78c1a2aa0bd0c61bc03383
Angel Huezo <angelohuezo@gmail.com>
2015-04-01 13:43:01 -0400

    code is complete. this imports from a file with headers

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
  ### --------- ####
 
 
-  get 'products/index'
+  # get 'products/index'
 
-  get 'products/import'
+  # get 'products/import'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
