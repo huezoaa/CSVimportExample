00cf8698e2fe8aefccb4f30421c94a7d6580d295
Angel Huezo <angelohuezo@gmail.com>
2015-04-01 20:02:14 -0400

    created postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150401164603) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "quantity"
